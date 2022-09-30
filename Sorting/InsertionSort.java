76ab68fe9e6b75d139b965346c9e8a18f06d9efa
zaidsidd360 <zaidsidd360@gmail.com>
2022-10-01 02:04:38 +0530

    Understood the most basic sorting techniques

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,36 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void sortArray(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+        System.out.print("The sorted array is: ");
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter a value: ");
+            arr[i] = sc.nextInt();
+        }
+
+        sortArray(arr);
+    }
+}
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,38 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void sortArray(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
+                }
+                int temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        System.out.print("The sorted array is: ");
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter a value: ");
+            arr[i] = sc.nextInt();
+        }
+
+        sortArray(arr);
+    }
+}
