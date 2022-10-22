2cfacd4e116a50a5643d9181ef49da4ed74c6424
zaidsidd360 <zaidsidd360@gmail.com>
2022-10-23 03:23:35 +0530

    I'm trying really hard here

--- OOPS/Penmaker.java
+++ OOPS/Penmaker.java
@@ -1,23 +1,36 @@
-class Penmaker {
-    public static void main(String[] args) {
-        Pen linc = new Pen();
-        linc.color = "blue";
-        linc.type = "ballpoint";
-        linc.write();
-        linc.printTypeAndColor();
-    }
-}
-
 class Pen {
     String color;
     String type;
 
-    public void write() {
-        String str = "Hi mom";
+    public void write(String str) {
         System.out.println(str);
     }
 
     public void printTypeAndColor() {
         System.out.println("I'm a " + this.type + " pen of " + this.color + " color.");
+    }
+}
+
+class Student {
+    String name;
+    int age;
+
+    public void printInfo() {
+        System.out.println("My name is " + this.name + " and I am " + this.age + " years old.");
+    }
+}
+
+public class Penmaker {
+    public static void main(String[] args) {
+        Pen linc = new Pen();
+        linc.color = "blue";
+        linc.type = "ballpoint";
+        linc.write("Hi mom!");
+        linc.printTypeAndColor();
+
+        Student s = new Student();
+        s.name = "Jack";
+        s.age = 20;
+        s.printInfo();
     }
 }
