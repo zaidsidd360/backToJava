87eb47f60a5551c39c505d98f4be8ed2ac73309e
zaidsidd360 <zaidsidd360@gmail.com>
2023-01-07 13:48:33 +0530

    prettified output

--- Miscellaneous/Square.java
+++ Miscellaneous/Square.java
@@ -4,7 +4,10 @@ public class Square {
     public static void printSquare(int n) {
         int pow = 1;
         for (int i = 0; i < n; i++) {
-            System.out.print((int) (Math.pow(5, pow)) + ", ");
+            if (i == n - 1)
+                System.out.println((int) (Math.pow(5, pow)) + ".");
+            else
+                System.out.print((int) (Math.pow(5, pow)) + ", ");
             pow++;
         }
     }
