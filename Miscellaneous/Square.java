af97c073bd3a6f30872d669e9bcf5a229340bf53
zaidsidd360 <zaidsidd360@gmail.com>
2023-01-07 13:44:05 +0530

    Optimised solution

--- Miscellaneous/Square.java
+++ Miscellaneous/Square.java
@@ -2,8 +2,7 @@ package Miscellaneous;
 
 public class Square {
     public static void printSquare(int n) {
-        int pow = 2;
-        System.out.print(5 + ", ");
+        int pow = 1;
         for (int i = 0; i < n; i++) {
             System.out.print((int) (Math.pow(5, pow)) + ", ");
             pow++;
