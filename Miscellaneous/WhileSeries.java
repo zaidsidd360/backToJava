8ecf5314b57e2479e4aafd3b667c6a5bf63d1b13
zaidsidd360 <zaidsidd360@gmail.com>
2023-01-10 09:28:12 +0530

    Spectrum

---
+++ Miscellaneous/WhileSeries.java
@@ -0,0 +1,21 @@
+package Miscellaneous;
+
+public class WhileSeries {
+    public static void printSeries(int n) {
+        int sum = 0;
+        int i = 1;
+        while (i <= n) {
+            sum += 2;
+            if (i == n)
+                System.out.print(sum + ".");
+            else
+                System.out.print(sum + ", ");
+            i++;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        printSeries(5);
+    }
+}
