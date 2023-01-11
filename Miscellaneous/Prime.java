c0f0d536a782bf646ac193934bb36c7a86c7032e
zaidsidd360 <zaidsidd360@gmail.com>
2023-01-11 17:38:08 +0530

    prime number

---
+++ Miscellaneous/Prime.java
@@ -0,0 +1,22 @@
+package Miscellaneous;
+
+public class Prime {
+
+    public static void checkPrime(int num) {
+        int factors = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0)
+                factors += 1;
+        }
+
+        if (factors == 2)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
+    }
+
+    public static void main(String[] args) {
+        checkPrime(7);
+    }
+}
