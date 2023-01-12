013140fd17a2bd3d967b6ef1f5f82af6d55334ae
zaidsidd360 <zaidsidd360@gmail.com>
2023-01-12 20:50:25 +0530

    Spectrum HW

---
+++ Miscellaneous/ClassMath.java
@@ -0,0 +1,35 @@
+package Miscellaneous;
+
+class Maths {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int diff(int a, int b) {
+        return a > b ? a - b : b - a;
+    }
+
+    public int prod(int a, int b) {
+        return a * b;
+    }
+
+    public double quo(int a, int b) {
+        return (double) a / b;
+    }
+
+    public int rem(int a, int b) {
+        return a % b;
+    }
+
+}
+
+public class ClassMath {
+    public static void main(String[] args) {
+        Maths m = new Maths();
+        int sum = m.sum(2, 3);
+        int diff = m.diff(2, 3);
+        int prod = m.prod(2, 3);
+        double quo = m.quo(2, 3);
+        System.out.println("Sum: " + sum + ", " + "diff: " + diff + ", " + "prod: " + prod + ", " + "quo: " + quo);
+    }
+}
