6c2074e145e6f325e81099d0b8700d5f9cd0dfb2
zaidsidd360 <zaidsidd360@gmail.com>
2023-01-13 19:15:44 +0530

    Practice

---
+++ Miscellaneous/PerfectSquare.java
@@ -0,0 +1,15 @@
+package Miscellaneous;
+import java.lang.Math;
+
+public class PerfectSquare {
+    public static void getPerfectSquares(int limit) {
+        for(int i = 1; i <= limit; i++) {
+            if(Math.sqrt(i) == (int) Math.sqrt(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        getPerfectSquares(100);
+    }
+}
