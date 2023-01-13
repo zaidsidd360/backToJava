1895c5ffdf2d2f4fc1deffdc9700986b46315715
zaidsidd360 <zaidsidd360@gmail.com>
2023-01-14 02:09:14 +0530

    Random

---
+++ Miscellaneous/GCD.java
@@ -0,0 +1,35 @@
+package Miscellaneous;
+
+public class GCD {
+    public static int[] bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static int findGCD(int a, int b) {
+        int gcd = 0;
+        for (int i = 1; i <= b; i++) {
+            if (a % i == 0 && b % i == 0) {
+                if (i > gcd)
+                    gcd = i;
+            }
+        }
+        return gcd;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 3, 6, 6, 7, 8 };
+        int sortedArray[] = bubbleSort(arr);
+        int largest = sortedArray[sortedArray.length - 1];
+        int smallest = sortedArray[0];
+        System.out.println(findGCD(smallest, largest));
+    }
+}
--- Miscellaneous/ThreeNplusOne.java
+++ Miscellaneous/ThreeNplusOne.java
@@ -3,16 +3,16 @@ package Miscellaneous;
 public class ThreeNplusOne {
     public static int calcIterations(int n) {
         int i = 0;
-        while(n != 1) {
-            if(n % 2 == 0) {
+        while (n != 1) {
+            if (n % 2 == 0)
                 n = n / 2;
-            } else {
+            else
                 n = (3 * n) + 1;
-            }
             i++;
         }
         return i;
     }
+
     public static void main(String[] args) {
         int iterations = calcIterations(97);
         System.out.println(iterations);
