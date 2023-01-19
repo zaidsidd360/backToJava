a4d79325f960a7e9db8262ac94c0fa36529c21ba
zaidsidd360 <zaidsidd360@gmail.com>
2023-01-19 16:22:30 +0530

    Submitted a more efficient solution for `Palindrome.java`

---
+++ LeetCode/Palindrome.java
@@ -0,0 +1,32 @@
+// Given an integer x, return true if x is a palindrome and false otherwise.
+
+package LeetCode;
+
+public class Palindrome {
+    public static boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int temp = x;
+        int rev = 0;
+        while (temp > 0) {
+            int rem = temp % 10;
+            rev = rev * 10 + rem;
+            temp = temp / 10;
+        }
+        return rev == x ? true : false;
+    }
+
+    public static boolean isPalindrome2(int x) {
+        String str = Integer.toString(x);
+        String compareStr = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            compareStr += str.charAt(i);
+        }
+        return str.equals(compareStr) ? true : false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(323));
+        System.out.println(isPalindrome2(121));
+    }
+}
