278756df1222d77d7d6423719005ff3115e04ace
zaidsidd360 <zaidsidd360@gmail.com>
2023-03-01 22:52:37 +0530

    Classwork

---
+++ SpectrumClasses/MobilePhone.java
@@ -0,0 +1,29 @@
+package SpectrumClasses;
+
+public class MobilePhone {
+	int modelNumber;
+	String modelName;
+
+	public MobilePhone(String s, int n) {
+		this.modelName = s;
+		this.modelNumber = n;
+	}
+
+	@Override
+	public boolean equals(Object m1) {
+		MobilePhone that = (MobilePhone) m1;
+		if (this.modelName == that.modelName && this.modelNumber == that.modelNumber)
+			return true;
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+		MobilePhone mp = new MobilePhone("Realme 6pro", 234423);
+		MobilePhone mp2 = new MobilePhone("Realme 6pro", 234423);
+
+		System.out.println(mp.equals(mp2));
+
+	}
+
+}
