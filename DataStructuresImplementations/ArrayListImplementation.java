f44f077f4cdf16220e1fa5451ad7f3d8b83bd69f
zaidsidd360 <zaidsidd360@gmail.com>
2023-03-07 04:37:08 +0530

    Completed `ArrayListImplementation.java`

--- DataStructuresImplementations/ArrayListImplementation.java
+++ DataStructuresImplementations/ArrayListImplementation.java
@@ -1,9 +1,9 @@
 package DataStructuresImplementations;
 
 class ArrayList<T> {
-	int capacity = 16;
-	int size = 0;
-	Object[] internalArray;
+	private int capacity = 16;
+	private int size = 0;
+	private Object[] internalArray;
 
 	/*
 	 * Parameterized constructor. Constructs a list with the given capacity
@@ -18,25 +18,64 @@ class ArrayList<T> {
 	}
 
 	/*
-	 * Non-parameterized constructor
+	 * Non-parameterized constructor.
 	 */
 	public ArrayList() {
 		internalArray = new Object[this.capacity];
 	}
 
 	/*
-	 * Adds the given value to the tail of the list. Complexity: Amortized O(1)
+	 * Appends the given element to the tail of the list. Complexity: Amortized O(1)
 	 */
 	public void add(T val) {
-		if (size == capacity - 1) {
+		if (size == capacity - 2) {
 			this.capacity *= 2;
-			Object[] tempArray = new Object[capacity];
-			for (int i = 0; i < this.size; i++) {
-				tempArray[i] = internalArray[i];
-			}
-			internalArray = tempArray;
+			this.resize(capacity);
 		}
 		this.internalArray[size++] = val;
+	}
+
+	/*
+	 * Inserts the given element at the specified index in the list. Complexity:
+	 * O(n)
+	 */
+	public void add(int index, T val) {
+		if (index >= size)
+			throw new IndexOutOfBoundsException();
+		if (size == capacity - 2) {
+			this.capacity *= 2;
+			this.resize(capacity);
+		}
+		for (int j = size + 1; j > index; j--) {
+			internalArray[j] = internalArray[j - 1];
+		}
+		internalArray[index] = val;
+		size++;
+	}
+
+	/*
+	 * Deletes the element present at the specified index from the list.
+	 */
+	public void delete(int index) {
+		if (index >= size)
+			throw new IndexOutOfBoundsException();
+		for (int i = index; i < size; i++) {
+			internalArray[i] = internalArray[i + 1];
+		}
+		size--;
+		if (size <= 0.25 * capacity)
+			capacity /= 2;
+	}
+
+	/*
+	 * Replaces all occurances of a specified value with the given value to be
+	 * replaced with.
+	 */
+	public void replace(T valToReplace, T valToBeReplacedWith) {
+		for (int i = 0; i < size; i++) {
+			if (internalArray[i].equals(valToReplace))
+				internalArray[i] = valToBeReplacedWith;
+		}
 	}
 
 	/*
@@ -44,9 +83,55 @@ class ArrayList<T> {
 	 * index is out of bounds. Complexity: O(1)
 	 */
 	public Object get(int index) {
-		if (index < 0 || index >= size)
+		if (index >= size)
 			throw new IndexOutOfBoundsException();
 		return internalArray[index];
+	}
+
+	/*
+	 * Replaces the specified element in the list, present at the specified index.
+	 * Throws indexOutOfBounds exception if given index is out of bounds.
+	 */
+	public void set(int index, T val) {
+		if (index >= size)
+			throw new IndexOutOfBoundsException();
+		internalArray[index] = val;
+	}
+
+	/*
+	 * Returns true if the list contains the given element, false otherwise.
+	 */
+	public boolean contains(T val) {
+		for (int i = 0; i < size; i++) {
+			if (internalArray[i].equals(val))
+				return true;
+		}
+		return false;
+	}
+
+	/*
+	 * Return the first index of the specified element, or -1 if the list does not
+	 * contain the element.
+	 */
+	public int indexOf(T val) {
+		for (int i = 0; i < size; i++) {
+			if (internalArray[i].equals(val))
+				return i;
+		}
+		return -1;
+	}
+
+	/*
+	 * Return the index of the last occurrence of the specified element, or -1 if
+	 * the list does not contain the element.
+	 */
+	public int lastIndexOf(T val) {
+		int ind = -1;
+		for (int i = 0; i < size; i++) {
+			if (internalArray[i].equals(val))
+				ind = i;
+		}
+		return ind;
 	}
 
 	/*
@@ -54,6 +139,22 @@ class ArrayList<T> {
 	 */
 	public boolean isEmpty() {
 		return size == 0;
+	}
+
+	/*
+	 * Returns the number of elements in the list.
+	 */
+	public int size() {
+		return size;
+	}
+
+	/*
+	 * Removes all elements from the list. Complexity: O(1)
+	 */
+	public void clear() {
+		this.capacity = 16;
+		this.size = 0;
+		internalArray = new Object[capacity];
 	}
 
 	/*
@@ -70,6 +171,18 @@ class ArrayList<T> {
 		System.out.print("]");
 		System.out.println();
 	}
+
+	/*
+	 * Private method to resize the internalArray if the size of list is at
+	 * capacity.
+	 */
+	private void resize(int newCapacity) {
+		Object[] tempArray = new Object[newCapacity];
+		for (int i = 0; i < this.size; i++) {
+			tempArray[i] = internalArray[i];
+		}
+		internalArray = tempArray;
+	}
 }
 
 public class ArrayListImplementation {
@@ -79,7 +192,12 @@ public class ArrayListImplementation {
 		names.add("Tausif");
 		names.add("Adib");
 		names.add("Maroof");
+		names.add("Maroof");
 		names.print();
-		System.out.println(names.get(1) + " " + names.isEmpty());
+		names.add(0, "Zaid");
+		names.add(3, "Nafis");
+		names.print();
+		System.out.println(names.indexOf("Maroof") + " " + names.lastIndexOf("Maroof") + " " + names.get(2) + " "
+				+ names.isEmpty() + " " + names.size() + " " + names.lastIndexOf("Waqqas"));
 	}
 }
