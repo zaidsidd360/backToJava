7d21c971bb957417da457df9fb18a7c4e3ab8f9f
zaidsidd360 <zaidsidd360@gmail.com>
2023-05-11 21:24:03 +0530

    Changes

---
+++ Miscellaneous/MergeSort.java
@@ -0,0 +1,12 @@
+package Miscellaneous;
+
+public class MergeSort {
+	public static void mergeSort(int[] array) {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- Miscellaneous/MergeTwoArrays.java
+++ Miscellaneous/MergeTwoArrays.java
@@ -1,26 +1,26 @@
 package Miscellaneous;
 
 public class MergeTwoArrays {
-    public static int[] mergeTwoArrays(int array1[], int array2[]) {
-        int mergedArray[] = new int[array1.length + array2.length];
-        int i;
+	public static int[] mergeTwoArrays(int array1[], int array2[]) {
+		int mergedArray[] = new int[array1.length + array2.length];
+		int i;
 
-        for (i = 0; i < array1.length; i++) {
-            mergedArray[i] = array1[i];
-        }
-        for (int j = 0; j < array2.length; j++) {
-            mergedArray[i] = array2[j];
-            i++;
-        }
-        return mergedArray;
-    }
+		for (i = 0; i < array1.length; i++) {
+			mergedArray[i] = array1[i];
+		}
+		for (int j = 0; j < array2.length; j++) {
+			mergedArray[i] = array2[j];
+			i++;
+		}
+		return mergedArray;
+	}
 
-    public static void main(String[] args) {
-        int arr1[] = { 1, 2, 3, 4, 5 };
-        int arr2[] = { 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
-        int mergedArray[] = mergeTwoArrays(arr1, arr2);
-        for (int e : mergedArray) {
-            System.out.println(e + " ");
-        }
-    }
+	public static void main(String[] args) {
+		int arr1[] = { 1, 2, 3, 4, 5 };
+		int arr2[] = { 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+		int mergedArray[] = mergeTwoArrays(arr1, arr2);
+		for (int e : mergedArray) {
+			System.out.print(e + " ");
+		}
+	}
 }
